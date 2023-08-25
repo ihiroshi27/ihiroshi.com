85a28a75bacb0e740f8c477362b30379484d4f32
Perayuth (Shi) Thanyanon <perayuth@digio.co.th>
2023-08-26 00:42:43 +0700

    update qr add hongkong and update layout

---
+++ qr/_next/static/cTh98NiopASJfMs-eEKT8/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/542-d15ca5debab47324.js","static/chunks/pages/index-218e253df494d538.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
